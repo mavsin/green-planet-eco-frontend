b3f0b96b6d7aea5c11712519202988891781701e
devnum1 <counter.offensive.devail@gmail.com>
2024-11-17 02:40:01 -0500

    feat: add payment gate way

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const COOKIE_KEYS = {
+    provider: '__nftt-provider'
+}
--- src/pages/CarbonCreditsPage/DialogMembership/CardMembership.tsx
+++ src/pages/CarbonCreditsPage/DialogMembership/CardMembership.tsx
@@ -1,19 +1,27 @@
+import React, { useState } from "react";
 import { Button, Card, CardBody } from "@material-tailwind/react";
 import { IMembership } from "../../../utils/interfaces";
+import { useNavigate } from 'react-router-dom';
+import WalletDialog from "../../WalletDialog";
 
 export default function CardMembership({ data }: { data: IMembership }) {
-  return (
-    <Card className="bg-primary">
-      <CardBody className="flex flex-col items-center justify-between gap-4 h-full">
-        <h5 className="text-center text-xl font-semibold text-white">
-          {data.title}
-        </h5>
-        <div className="flex flex-col items-center justify-between gap-4 text-white">
-          <span className="text-3xl md:text-4xl font-bold">${data.price}</span>
-          <p className="text-sm md:text-base text-center">{data.description}</p>
-          <Button className="bg-white text-primary">Purchase</Button>
-        </div>
-      </CardBody>
-    </Card>
+  const [isOpen, setIsOpen] = useState(false);
+  let navigate = useNavigate();
+
+  return (<>
+      <Card className="bg-primary">
+        <CardBody className="flex flex-col items-center justify-between gap-4 h-full">
+          <h5 className="text-center text-xl font-semibold text-white">
+            {data.title}
+          </h5>
+          <div className="flex flex-col items-center justify-between gap-4 text-white">
+            <span className="text-3xl md:text-4xl font-bold">${data.price}</span>
+            <p className="text-sm md:text-base text-center">{data.description}</p>
+            <Button className="bg-white text-primary" onClick={() => setIsOpen(true)}>Purchase</Button>
+          </div>
+        </CardBody>
+      </Card>
+      <WalletDialog isOpen={isOpen} handleClose={() => navigate('/')} />
+    </>
   );
 }
